63ac500058cb462244f6429aede284dfa849b977
Tomas Naiouf <tomasnaiouf@gmail.com>
2021-09-13 12:46:30 -0300

    Conceptos de rutas

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World"
+@app.route("/home/<string:name>/posts/<int:id>")
+def hello(name, id):
+    return "Hello, " + name + ", your post ID is " + str(id)
+
+@app.route("/onlyget", methods = ["POST"])
+def get_req():
+    return "You can only get this!"
 
 if __name__ == "__main__":
     app.run(debug=True)
